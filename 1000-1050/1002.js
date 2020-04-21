6c25eb4bbf0479c1cf170af7406ae600b6ce9262
liangchaofei <chaofei_liang@126.com>
2020-04-21 21:50:42 +0800

    add 1002

---
+++ 1000-1050/1002.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: your name
+ * @Date: 2020-04-21 21:29:24
+ * @LastEditTime: 2020-04-21 21:29:45
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /leetcode/1000-1050/1002.js
+ */
+var arr = ["bella","label","roller"];
+var commonChars = function(A) {
+
+};
